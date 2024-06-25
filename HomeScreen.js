4a19ed576dbcc4d169cdb3f1af98b6f0c1f754c7
A-v-polunin <polunin-sasha2003@mail.ru>
2024-06-25 23:20:03 +0300

    3 laba

--- HomeScreen.js
+++ HomeScreen.js
@@ -3,6 +3,7 @@ import { View, Text, Button } from 'react-native';
 
 
 const HomeScreen = ({ navigation }) => {
+  const [currentTime, setCurrentTime] = useState('');
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -16,7 +17,8 @@ const HomeScreen = ({ navigation }) => {
   }, []);
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' , gap: '20px'}}>
-      <Text style={{ fontSize: '30px' }}>2 Лабараторная работа</Text>
+      <Text style={{ fontSize: 15 }}>Часы: {currentTime}</Text>
+      <Text style={{ fontSize: '30px' }}>Полунин Александр Вячеславович, группа 211-323</Text>
       <Text style={{ fontSize: '20px' }}>Home Screen</Text>
       <Button
         title="Go to Profile"
